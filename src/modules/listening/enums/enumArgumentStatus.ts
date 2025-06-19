65cc36226cc3d0a9d899b872977fc1b61c76c5ba
chaoting <chaoting666@gmail.com>
2025-06-20 02:10:43 +0800

    fix: enumArgumentStatus

--- src/modules/listening/enums/enumArgumentStatus.ts
+++ src/modules/listening/enums/enumArgumentStatus.ts
@@ -3,9 +3,10 @@ export enum EnumArgumentStatus {
   HP100 = 'HP100',                             // 完全成立
   HP90 = 'HP90',                               // 削弱
   HP10 = 'HP10',                               // 極大削弱
-  HP0Obfuscation = 'HP0Obfuscation',         // 擊倒(打糊)
   HP0InitFail = 'HP0InitFail',               // 初步不成立
+  HP0Obfuscation = 'HP0Obfuscation',         // 擊倒(打糊)
   HP0Disassembly = 'HP0Disassembly',         // 擊倒(拆掉)
+  HP0Countermeasures = 'HP0Countermeasures', // 擊倒(相抗)
   Lose = 'Lose',                               // 掉點
   Unknown = 'Unknown',                         // 未知
   Other = 'Other',                             // 其他
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import eslint from 'vite-plugin-eslint';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 import packageFile from './package.json';
-import vitePrerender from 'vite-plugin-prerender';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,12 +15,6 @@ export default defineConfig({
     viteTsconfigPaths(),
     eslint({ include: 'src' }),
     svgr(),
-    vitePrerender({
-      // Required - The path to the vite-outputted app to prerender.
-      staticDir: path.join(__dirname, 'dist'),
-      // Required - Routes to render.
-      routes: ['/', '/listenings', '/listening', '/topicCreator'],
-    }),
   ],
   server: {
     port: 3000,
